9fb3cfc54ce96cfb049a999b31b8842cca5cc079
Joy Shaffer <jeshaffer@anderson.edu>
2017-06-29 10:31:36 -0400

    Added favorite and delete buttons

--- README.md
+++ README.md
@@ -2,3 +2,19 @@
 
 A catalog of Michael Bay's ridiculous filmography.
 
+Day 3 HW
+*add flicks to array(1line)
+
+ bonus:
+*add a fav or like button to each list item
+*make it change appearance of that item(background color)
+
+mega bonus:
+*add a remove button to each list item
+*out of list and ideally out of the array
+
+super mega bonues:
+*have both buttons affect the array not just the DOM
+*make fav button toggle the fav status
+
+*add buttons that move a flick up and down the list
--- app.js
+++ app.js
@@ -1,34 +1,69 @@
 const app = {
-    init(selectors) {
-        this.max = 0
-        this.list = document.querySelector(selectors.listSelector)
-        document
-            .querySelector(selectors.formSelector)
-            .addEventListener('submit', this.handleSubmit.bind(this))
-    },
+  init(selectors) {
+      this.flicks = []
+    this.max = 0
+    this.list = document.querySelector(selectors.listSelector)
+
+    document
+      .querySelector(selectors.formSelector)
+      .addEventListener(
+        'submit', 
+        this.handleSubmit.bind(this)
+      )
+  },
+
+  renderListItem(flick) {
+    const item = document.createElement('li')
+    item.textContent = flick.name
+    item.style.backgroundColor = "lightgrey"
+    const favButton = document.createElement('button')
+    const delButton = document.createElement('button')
+    //favButton.getAttribute('id', 'f$(flick.id)')
+    //favButton.getAttribute('id', 'f$(flick.id)')
+    favButton.textContent = 'fav'
+    delButton.textContent = 'delete'
+    favButton.style.backgroundColor = 'red'
+    delButton.style.backgroundColor = 'yellow'
+    favButton.style.position = 'absolute'
+    delButton.style.position = 'absolute'
+    favButton.style.right = '230px'
+    delButton.style.right = '290px'
+    favButton.style.height = '25px'
+    favButton.style.width = '60px'
+    delButton.style.height = '25px'
+    delButton.style.width = '60px'
+    favButton.addEventListener('click', ()=>{
+        if(item.style.backgroundColor == 'lightgrey') {
+            item.style.backgroundColor = 'gold'}
+            else{item.style.backgroundColor = 'lightgrey'}})
+        
     
-    renderListItem(flick) {
-        const item = document.createElement('li')
-        item.textContent = flick.name
-        return item
-    },
+    
+    delButton.addEventListener('click', ()=>{item.parentNode.removeChild(item)})
+    item.appendChild(favButton)
+    item.appendChild(delButton)
+    return item
+  },
 
-    handleSubmit(ev) {
-        ev.preventDefault()
-        const f = ev.target
-        const flick = {
-            id: this.max + 1, 
-            name: f.flickName.value,
-        }
+  handleSubmit(ev) {
+    ev.preventDefault()
+    const f = ev.target
+    const flick = {
+      id: this.max + 1,
+      name: f.flickName.value,
+    }
 
-        const listItem = this.renderListItem(flick)
-        this.list.appendChild(listItem)
-
-        this.max++
-    },
+    const listItem = this.renderListItem(flick)
+    this.list.appendChild(listItem)
+    this.flicks[flick.id - 1] = flick.Name
+    this.max ++
+  }
 }
 
+
+
+
 app.init({
-    formSelector: 'form#flick-form',
-    listSelector: '#flick-list',
- })
+  formSelector: 'form#flick-form',
+  listSelector: '#flick-list',
+})
