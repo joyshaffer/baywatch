29828f9f5a35b72bae36c4bccb5f931d84ce0186
Joy Shaffer <jeshaffer@anderson.edu>
2017-07-05 11:38:49 -0400

    finished project

--- app.js
+++ app.js
@@ -1,118 +1,169 @@
-const app = {
-  init(selectors) {
-      this.flicks = []
+class App {
+  constructor(selectors) {
+    this.flicks = []
     this.max = 0
     this.list = document.querySelector(selectors.listSelector)
     this.template = document.querySelector(selectors.templateSelector)
 
     document
       .querySelector(selectors.formSelector)
-      .addEventListener(
-        'submit', 
-        this.handleSubmit.bind(this)
-      )
-  },
+      .addEventListener('submit', this.handleSubmit.bind(this))
+
+    this.load()
+  }
+
+  load() {
+    const flicksJSON = localStorage.getItem('flicks')
+    const flicksArray = JSON.parse(flicksJSON)
+
+    if (flicksArray) {
+      flicksArray
+        .reverse()
+        .map(this.addFlick.bind(this))
+    }
+  }
+
+  save() {
+    localStorage.setItem('flicks', JSON.stringify(this.flicks))
+  }
+
+  saveOnEnter(flick, listItem, ev) {
+    if (ev.key === 'Enter') {
+      this.toggleEditable(flick, listItem)
+    }
+  }
+
+  toggleEditable(flick, listItem, ev) {
+    const nameField = listItem.querySelector('.flick-name')
+    const btn = listItem.querySelector('button.edit')
+    const icon = btn.querySelector('i.fa')
+
+    if (nameField.isContentEditable) {
+      // make it no longer editable
+      nameField.contentEditable = false
+      icon.classList.remove('fa-check')
+      icon.classList.add('fa-pencil')
+      btn.classList.remove('success')
+
+      // save changes
+      flick.name = nameField.textContent
+      this.save()
+    } else {
+      // make it editable
+      nameField.contentEditable = true
+      nameField.focus()
+      icon.classList.remove('fa-pencil')
+      icon.classList.add('fa-check')
+      btn.classList.add('success')
+    }
+  }
+
+  favFlick(flick, ev) {
+    const listItem = ev.target.closest('.flick')
+    flick.fav = listItem.classList.toggle('fav')
+
+    this.save()
+  }
 
   removeFlick(flick, ev) {
-    //remove from the DOM
+    // remove from the DOM
     const listItem = ev.target.closest('.flick')
     listItem.remove()
 
-    //remove from the array
+    // remove from the array
     const i = this.flicks.indexOf(flick)
     this.flicks.splice(i, 1)
 
     this.save()
-  },
+  }
 
-  favFlick(flick,ev) {
-      const listItem = ev.target.closest('.flick')
-      flick.fav = !flick.fav
+  moveDown(flick) {
+    const i = this.flicks.indexOf(flick)
 
-      //flick.fav = listItem.classList.toggle('fav')
+    if (i < this.flicks.length - 1) {
+      this.moveUp(this.flicks[i + 1])
+    }
+  }
 
-      if(flick.fav) {
-        listItem.classList.add('fav')
-      } else {
-        listItem.classList.remove('fav')
-      }
+  moveUp(flick) {
+    const listItem = this.list.querySelector(`[data-id="${flick.id}"]`)
+    const i = this.flicks.indexOf(flick)
 
-      this.save()
-  },
+    if (i > 0) {
+      this.list.insertBefore(listItem, listItem.previousElementSibling)
 
-  moveUp(flick,ev) {
-    const listItem = ev.target.closest('.flick')
-    const flickArr = this.flicks[flickName.value]
+      const previousFlick = this.flicks[i - 1]
+      this.flicks[i - 1] = flick
+      this.flicks[i] = previousFlick
 
-    const index = flickArr.findIndex((currentFlick, i) => {
-        return currentFlick.id === flick.id
-    })
-
-    if (index > 0) {
-      this.list[flickName.value].insertBefore(listItem, listItem.previousElementSibling)
-
-      const previousFlick = flickArr[index - 1]
-      flickArr[index - 1] = flick
-      flickArr[index] = previousFlick
       this.save()
     }
-  },
+  }
+addFlick(flick) {
+    this.flicks.unshift(flick)
 
-  moveDown(flick, ev) {
-    const listItem = ev.target.closest('.flick')
-    const flickArr = this.flicks[flickName.value]
+    const listItem = this.renderListItem(flick)
+    this.list.insertBefore(listItem, this.list.firstElementChild)
 
-    const index = flickArr.findIndex((currentFlick, i) => {
-        return currentFlick.id === flick.id
-    })
-
-    if (index < flickArr.length - 1) {
-      this.list[flickName.value].insertBefore(listItem.nextElementSibling, listItem)
-      
-      const nextFlick = flickArr[index + 1]
-      flickArr[index + 1] = flick
-      flickArr[index] =  nextFlick
-      this.save()
+    if (flick.id > this.max) {
+      this.max = flick.id
     }
-  },
+    this.save()
+  }
 
   renderListItem(flick) {
     const item = this.template.cloneNode(true)
     item.classList.remove('template')
     item.dataset.id = flick.id
-    item
-        .querySelector('.flick-name')
-        .textContent = flick.name
+
+    if (flick.fav) {
+      item.classList.add('fav')
+    }
+
+    const nameSpan = item.querySelector('.flick-name')
+    nameSpan.textContent = flick.name
+    nameSpan.addEventListener(
+      'keypress',
+      this.saveOnEnter.bind(this, flick, item)
+    )
 
     item
-        .querySelector('button.remove')
-        .addEventListener(
-            'click',
-            this.removeFlick.bind(this, flick)
-         )
+      .querySelector('button.remove')
+      .addEventListener(
+        'click',
+        this.removeFlick.bind(this, flick)
+      )
 
     item
-        .querySelector('button.fav')
-        .addEventListener(
-            'click',
-            this.favFlick.bind(this, flick)
-        )
+      .querySelector('button.fav')
+      .addEventListener(
+        'click',
+        this.favFlick.bind(this, flick)
+      )
 
     item
-        .querySelector('button.up')
-        .addEventListener(
-            'click',
-            this.moveUp.bind(this, flick)
-        )
+      .querySelector('button.move-up')
+      .addEventListener(
+        'click',
+        this.moveUp.bind(this, flick)
+      )
 
     item
-        .querySelector('button.down')
-        .addEventListener(
-            'click',
-            this.moveDown.bind(this, flick)
-        )
+      .querySelector('button.move-down')
+      .addEventListener(
+        'click',
+        this.moveDown.bind(this, flick)
+      )
+
+    item
+      .querySelector('button.edit')
+      .addEventListener(
+        'click',
+        this.toggleEditable.bind(this, flick, item)
+      )
+
     return item
-  },
+  }
 
   handleSubmit(ev) {
     ev.preventDefault()
@@ -122,20 +173,15 @@ const app = {
       name: f.flickName.value,
       fav: false,
     }
-    this.flicks.unshift(flick)
 
-    const listItem = this.renderListItem(flick)
-    this.list.insertBefore(listItem, this.list.firstElementChild)
-
-    //this.flicks[flick.id - 1] = flick.Name
-    this.max ++
+    this.addFlick(flick)
 
     f.reset()
   }
 }
 
-app.init({
+const app = new App({
   formSelector: 'form#flick-form',
   listSelector: '#flick-list',
-  templateSelector: '.flick.template',
+  templateSelector: '.flick.template'
 })
